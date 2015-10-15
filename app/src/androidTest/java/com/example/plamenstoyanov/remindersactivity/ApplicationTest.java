f38919e560a843df29699ceb2fe0a8d638e285ad
Plamen.Stoyanov <plamen1967@gmail.com>
2015-10-15 14:03:40 +0100

    Initial Commit

--- app/src/androidTest/java/com/example/plamenstoyanov/remindersactivity/ApplicationTest.java
+++ app/src/androidTest/java/com/example/plamenstoyanov/remindersactivity/ApplicationTest.java
@@ -1,13 +1,25 @@
 package com.example.plamenstoyanov.remindersactivity;
 
 import android.app.Application;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends InstrumentationTestCase
+{
+    private UiDevice mDevice;
+
+    public void setUp() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
     }
+
 }
